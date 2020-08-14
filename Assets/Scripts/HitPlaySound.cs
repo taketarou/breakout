530e5a732a34bee7ae04f6ce6017735fba1b42d6
taketarou <takeda35679@gmail.com>
2020-08-15 08:14:51 +0900

    ボールが物に当たった時に音を再生

---
+++ Assets/Scripts/HitPlaySound.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPlaySound : MonoBehaviour
+{
+
+    public AudioClip sound; //AudioClipクラス型の参照型変数soundにはこのスクリプトが適用されているゲームオブジェクトのInspectorタブのSoundに指定したsoundのID番号が代入される。
+
+    //このスクリプトが適用されているゲームオブジェクトにオブジェクトがあたってきたとき、この関数は呼ばれる。あたってきたオブジェクトのID番号が引数として、参照型変数collisionに代入される。
+    void OnCollisionEnter(Collision collision)
+    {
+        //このスクリプトが適用されているゲームオブジェクトと同じ位置で、soundに格納されているID番号が指すオブジェクト(=効果音)を鳴らす(繰り返しなし)
+        AudioSource.PlayClipAtPoint(sound, transform.position);
+    }
+
+}
