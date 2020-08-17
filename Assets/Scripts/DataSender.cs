68581a8e65291575b7f7e223744097a908c0d37f
taketarou <takeda35679@gmail.com>
2020-08-17 18:10:01 +0900

    コメント追加

--- Assets/Scripts/DataSender.cs
+++ Assets/Scripts/DataSender.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//StartやUpdateといった関数が必要ないからMonoBehaviourはつけない
+//StartやUpdateといった関数が必要ないからMonoBehaviourはつけない 　unityのstaticはstart関数やupdate関数を使う必要がないスクリプトで主に使う。
 public static class DataSender　
 {
     public static string resultMessage;
