8f3f0bb137517bc0e71b7b6631fd04fbf89e8fa5
taketarou <takeda35679@gmail.com>
2020-08-17 18:11:41 +0900

    スコアとハイスコアの表示

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Destroyer : MonoBehaviour
 {
+    public int point;
     public GameObject masterObj; //InspecterタブのMaster Objに指定したオブジェクトのID番号が格納されるが、この場合は、BoxInitスクリプトを通してMasterゲームオブジェクトのID番号が格納される。
 
     // Start is called before the first frame update
@@ -18,9 +19,11 @@ public class Destroyer : MonoBehaviour
         
     }
 
-    
-    private void OnCollisionEnter(Collision collision)
+    //このスクリプトが適用されているゲームオブジェクト(=ブロック)にオブジェクトがあたってきたとき、この関数は呼ばれる。あたってきたオブジェクトのID番号が引数として、参照型変数collisionに代入される。
+    private void OnCollisionEnter(Collision collision) //privateなしでもいい？
     {
+        FindObjectOfType<Score>().AddPoint(point); //ScoreスクリプトのAddPoint関数を呼び出し、スコアを加算する。　FindObjectOfTypeは主にコンポーネントの型を取るときに使う。
+
         //masterObjが示すゲームオブジェクト(=この場合は、Masterゲームオブジェクト)のGameMasterコンポーネント(=GameMasterスクリプト)の変数boxNumを－1する。
         masterObj.GetComponent<GameMaster>().boxNum--;
 
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour //ScoreCanvasオブジェクトにアタッチされるスクリプト
+{
+
+    // スコアを表示するUIの取得用
+    public Text scoreText; //ScoreTextゲームオブジェクトのID番号が代入される
+    // ハイスコアを表示するUIの取得用
+    public Text highScoreText; //HighScoreTextゲームオブジェクトのID番号が代入される
+
+    // スコアのカウント用
+    private int score; 
+
+    // ハイスコアのカウント用
+    private int highScore; //初期化されていないので、初期値として自動的に0が代入される。
+
+    // PlayerPrefsで保存するためのキー(=データの保存、読み込み用のキー)
+    private string highScoreKey = "highScore";
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    // ゲーム開始前の状態に設定
+    private void Initialize()
+    {
+        // スコアを0に設定
+        score = 0;
+
+        // highScoreKeyに保存されているint型の値（＝過去の最高スコア・ハイスコア）を取得し、それをhighScoreへ代入する。highScoreKeyに値が保存されていなければ(=過去にブロック崩しをやっていなければ)、0を取得する。
+        highScore = PlayerPrefs.GetInt(highScoreKey, 0);
+    }
+
+    // ポイントの追加。修飾子をpublicにしているので外部より参照できるメソッドになっている
+    public void AddPoint(int point)　　　　　　//　外部より受け取ったint型の引数(=今回は10)をpointとして受け取る
+    {
+        score += point; //score=score+point
+
+        // スコアがハイスコアより大きくなれば、ハイスコアを更新する
+        if (highScore < score)
+        {
+            highScore = score;
+        }
+
+        // ゲーム画面上のスコアとハイスコアの表示を更新する
+        DisplayScores();
+    }
+
+    // ゲーム画面上のスコアとハイスコアの表示を更新する
+    private void DisplayScores()
+    {
+        // 現在のスコアとハイスコアを画面に表示する
+        scoreText.text = score.ToString(); //ScoreTextゲームオブジェクトの文字列をscore(ToString関数でString型に変換されている)に格納されている値にする。
+        highScoreText.text = highScore.ToString(); //HighScoreTextゲームオブジェクトの文字列をhighScore(ToString関数でString型に変換されている)に格納されている値にする。
+    }
+
+    // ハイスコアの保存
+    public void Save()
+    {
+        // ハイスコアを保存する
+        PlayerPrefs.SetInt(highScoreKey, highScore);//highScoreKeyというString型の変数に、highScoreに格納されているint型の値を保存する。
+        PlayerPrefs.Save(); //保存を確定する(これによって、ゲームが終了しても(＝プログラムが終了しても)、highScoreに格納されたハイスコアをhighScoreKeyという変数を通して参照できるようになる)。
+
+        // ゲーム開始前の状態に戻す
+        Initialize();
+    }
+}
+
