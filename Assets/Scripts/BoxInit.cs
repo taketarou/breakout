aa9502fe28c822e8b1a6c30deee327a4bbae585a
taketarou <takeda35679@gmail.com>
2020-08-22 10:15:18 +0900

    異なる色のブロックを作り、配置

--- Assets/Scripts/BoxInit.cs
+++ Assets/Scripts/BoxInit.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BoxInit : MonoBehaviour
 {
     //ゲームオブジェクトクラス型の参照型変数を定義。ゲームオブジェクトクラス型の参照型変数を定義すると、このスクリプトをアタッチしたゲームオブジェクトのInspectorタブに、定義したゲームオブジェクト型の参照型変数と同じ名前の項目が追加される。そして、Inspectorタブ上のBox Obj Prefabに指定したBoxのPrefabのID番号はboxObjPrefab変数に、Boxes Objに指定したBoxesオブジェクトのID番号はboxesObjオブジェクト変数にそれぞれ格納される。
-    public GameObject boxObjPrefab; 
+    public GameObject[] boxObjPrefabs; 
     public GameObject boxesObj;
 
     //AwakeとはStartよりも早く呼ばれるイベント関数です．Awake関数内の処理はStartよりも先に実行されます．
@@ -18,8 +18,12 @@ public class BoxInit : MonoBehaviour
         {
             for (int y = 0; y < 5; y++) 
             {
+                int randomValue = Random.Range(0, boxObjPrefabs.Length);
+
+                GameObject g = Instantiate(boxObjPrefabs[randomValue], boxesObj.transform);
+
                 //BoxのPrefab(=boxobjPrefabに格納されているID番号が示すオブジェクト)と同じものを複製する(複製してできたBoxオブジェクトは、Boxesオブジェクト(=boxesobjに格納されているID番号が示すオブジェクト)の子オブジェクトとなる)。そして、その複製したオブジェクト(ゲームオブジェクト)のID番号をGameObjectクラス型の参照型変数gに格納する。 　Instantiate関数は、オブジェクトを基にオブジェクトを生成する(=オブジェクトを複製する)関数。
-                GameObject g = Instantiate(boxObjPrefab, boxesObj.transform); //gはprefabと同じ位置に作られる。
+                //GameObject g = Instantiate(boxObjPrefab, boxesObj.transform); //gはprefabと同じ位置に作られる。
 
                 //gに格納されているID番号が指すオブジェクト(=Boxを複製したもの)のtransformのposition(のx,y,z)に2+(1 * y),0.4、-4.2+ (1.2 * x)をそれぞれ代入する。 Vector3の引数は全部float型。
                 g.transform.position = new Vector3((2f + (1f * y)), 0.4f, (-4.2f + (1.2f * x)));　//prefabのxが4でboxesのxが2のときgのxは0  prefabのxが0でboxesのxが2のときgのxは0   prefab0 boxes4のとき-2  prefab2 boxes4のとき-2　 prefab0 boxes0のとき2   prefab6 boxes0のとき2  親の座標が0なら子の座標に関わらず、計算した結果の座標が正しく代入される。また親の座標が一緒なら子の座標が違くてもgには同じ座標が代入される。
--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -7,10 +7,17 @@ public class Destroyer : MonoBehaviour
     public int point;
     public GameObject masterObj; //InspecterタブのMaster Objに指定したオブジェクトのID番号が格納されるが、この場合は、BoxInitスクリプトを通してMasterゲームオブジェクトのID番号が格納される。
 
+    public int initHp; //耐久力の最大値
+    private int currentHp; //耐久力の現在値
+
+    public AudioClip hitSE; //破壊されない場合のSE
+    public AudioClip DestroySE; //破壊された場合のSE
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Boxの耐久力を初期化(現在値＝最大値にする)
+        this.currentHp = initHp;
     }
 
     // Update is called once per frame
@@ -22,11 +29,35 @@ public class Destroyer : MonoBehaviour
     //このスクリプトが適用されているゲームオブジェクト(=ブロック)にオブジェクトがあたってきたとき、この関数は呼ばれる。あたってきたオブジェクトのID番号が引数として、参照型変数collisionに代入される。
     private void OnCollisionEnter(Collision collision) //privateなしでもいい？
     {
+        //ボールが接触する度に耐久力を1ずつ減らす
+        this.currentHp -= 1;
+
+        //  壊れる場合
+        if (this.currentHp <= 0)
+        {
+            //  破壊されるSEを再生
+            AudioSource.PlayClipAtPoint(DestroySE, transform.position);
+            // 残りのブロック数を１つ減らす
+            masterObj.GetComponent<GameMaster>().boxNum--;
+            // スコアを加算する
+            FindObjectOfType<Score>().AddPoint(point);
+            // このゲームオブジェクト（ブロック）を破壊する　　　　
+            Destroy(this.gameObject);
+        }
+        //  まだ壊れない場合
+        else
+        {
+            //  ボールを跳ね返すSEを再生
+            AudioSource.PlayClipAtPoint(hitSE, transform.position);
+        }
+
+        /*異なる色のブロックを作るでは不要
         FindObjectOfType<Score>().AddPoint(point); //ScoreスクリプトのAddPoint関数を呼び出し、スコアを加算する。　FindObjectOfTypeは主にコンポーネントの型を取るときに使う。
 
         //masterObjが示すゲームオブジェクト(=この場合は、Masterゲームオブジェクト)のGameMasterコンポーネント(=GameMasterスクリプト)の変数boxNumを－1する。
         masterObj.GetComponent<GameMaster>().boxNum--;
 
         Destroy(gameObject);
+        */
     }
 }
