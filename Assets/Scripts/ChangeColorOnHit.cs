6dbce81efabc4ad5a356ce2e10dda85627771adf
taketarou <takeda35679@gmail.com>
2020-10-17 14:28:15 +0900

    弾が当たるごとにブロックの色を変化させる

---
+++ Assets/Scripts/ChangeColorOnHit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ChangeColorOnHit : MonoBehaviour
+{
+    //Hierarchyからカラーをセットするために配列を用意する
+    //public Color[] blockColors;
+
+    //HierarchyからカラーをセットするためにMaterialクラス型の配列を用意する。blockColors[0]には灰色を表すGrayMaterialのID番号、blockColors[1]には青色を表すBlueMaterialのID番号が代入される。
+    public Material[] blockColors; //配列blockColorsの要素数は2
+
+    /// <summary>
+    /// ブロックの色を変更するメソッド。配列のSizeで指定した数が、色の変わる順番と内容になる
+    /// </summary>
+    /// <param name="index">ブロックの残りのHP</param>
+    /// <returns>色</returns>
+    public Material ChangeMaterialColor(int index) //indexにはcureentHp(=2、または1)が代入される。
+    {
+        //  ブロックの色を配列に登録されているIndexの色に変更する。indexが2のときにはBlueMaterial、1のときにはGrayMaterialを返す。
+        return blockColors[index-1]; 
+    }
+}
--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -2,22 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//BoxプレファブとHardBoxプレファブにアタッチされるスクリプト(Awakeメソッドが存在するBoxInitスクリプトが実行されてからこのスクリプトが実行される。その場合、BoxInitスクリプトによってBoxプレファブのCloneとHardBoxプレファブのClone(Destroyerスクリプトがアタッチされている)が生成されているので、Destryoerスクリプトのフィールドには各CloneのDestroyerコンポーネント欄の項目に指定されている値が代入される)
 public class Destroyer : MonoBehaviour
 {
     public int point;
     public GameObject masterObj; //InspecterタブのMaster Objに指定したオブジェクトのID番号が格納されるが、この場合は、BoxInitスクリプトを通してMasterゲームオブジェクトのID番号が格納される。
 
-    public int initHp; //耐久力の最大値
+    public int initHp; //耐久力の最大値　Box(Clone)の場合1、HardBox(Clone)の場合2が代入される。
     private int currentHp; //耐久力の現在値
 
     public AudioClip hitSE; //破壊されない場合のSE
     public AudioClip DestroySE; //破壊された場合のSE。button01aというSEのID番号が格納される。
+
+    public Renderer rend; //ブロックの色の変更用。Box(Clone)のID番号、または、HardBox(Clone)のID番号が代入される。
+    private ChangeColorOnHit changeColorOnHit; //色を変更するChangeColorOnHitクラスの情報を代入する参照型変数
 
     // Start is called before the first frame update
     void Start()
     {
         //Boxの耐久力を初期化(現在値＝最大値にする)
         this.currentHp = initHp;
+
+        //Masterゲームオブジェクトが持っているChangeColorOnHitコンポーネント(=ChangeColorOnHitスクリプト)を取得し、そのID番号をchangeColorOnHit変数に代入する。
+        changeColorOnHit = masterObj.GetComponent<ChangeColorOnHit>();
+
+        //ChangeColorOnHitコンポーネント(=ChangeColorOnHitスクリプト)のChangeMaterialColor関数を呼び出し、その戻り値(=BlueMaterial、またはGrayMaterial)をBox(Clone)または、HardBox(Clone)のMesh RendererコンポーネントのMaterialsのElement0に代入する。
+        rend.material= changeColorOnHit.ChangeMaterialColor(currentHp);
     }
 
     // Update is called once per frame
@@ -32,7 +42,7 @@ public class Destroyer : MonoBehaviour
         //ボールが接触する度に耐久力を1ずつ減らす
         this.currentHp -= 1;
 
-        //  壊れる場合
+        //  壊れる場合(=現在の耐久力が0以下の場合)
         if (this.currentHp <= 0)
         {
             //  破壊されるSEを再生(このスクリプトがアタッチされているブロックゲームオブジェクトのTransformコンポーネントのPositionの位置で、DestroySEに格納されているID番号が指すbutton01Aを鳴らす)
@@ -44,11 +54,14 @@ public class Destroyer : MonoBehaviour
             // このゲームオブジェクト（ブロック）を破壊する　　　　
             Destroy(this.gameObject);
         }
-        //  まだ壊れない場合
+        //  まだ壊れない場合(=現在の耐久力が1以上の場合)
         else
         {
             //  ボールを跳ね返すSEを再生
             AudioSource.PlayClipAtPoint(hitSE, transform.position);
+
+            //ChangeBlockColorメソッドを呼び出し、ブロックの色を変更
+            ChangeBlockColor();
         }
 
         /*異なる色のブロックを作るでは不要
@@ -59,5 +72,11 @@ public class Destroyer : MonoBehaviour
 
         Destroy(gameObject);
         */
+    }
+
+
+    private void ChangeBlockColor()
+    {
+        rend.material = changeColorOnHit.ChangeMaterialColor(currentHp);　//ChangeColorOnHitコンポーネント(=ChangeColorOnHitスクリプト)のChangeMaterialColor関数を呼び出し、その戻り値(=BlueMaterial、またはGrayMaterial)をBox(Clone)または、HardBox(Clone)のMesh RendererコンポーネントのMaterialsのElement0に代入する。
     }
 }
